a7ec7081b0c28e160e7bdd4a47cfc9cae324618b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-06-02 12:09:06 +0200

    Published new version

--- random_dict/__init__.py
+++ random_dict/__init__.py
@@ -1,3 +1,4 @@
 from .random_dict import random_dict, random_float_dict, random_bool_dict, random_string_dict, random_int_dict
 
-__all__ = ["random_dict", "random_float_dict", "random_bool_dict", "random_string_dict", "random_int_dict"]
+__all__ = ["random_dict", "random_float_dict",
+           "random_bool_dict", "random_string_dict", "random_int_dict"]
