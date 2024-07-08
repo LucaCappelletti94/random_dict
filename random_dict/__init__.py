5595801eca36cdacf49777dd0240e9b45f7cc90b
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2024-07-08 17:12:51 +0200

    Exposed generators

--- random_dict/__init__.py
+++ random_dict/__init__.py
@@ -1,10 +1,20 @@
 """Random-Dict module."""
-from .random_dict import (
+from random_dict.random_dict import (
     random_dict,
     random_float_dict,
     random_bool_dict,
     random_string_dict,
     random_int_dict,
+)
+
+from random_dict.random_dict import (
+    random_bool,
+    random_float,
+    random_int,
+    random_bytes,
+    random_string,
+    random_dataframe,
+    random_numpy_array
 )
 
 __all__ = [
@@ -13,4 +23,11 @@ __all__ = [
     "random_bool_dict",
     "random_string_dict",
     "random_int_dict",
+    "random_bool",
+    "random_float",
+    "random_int",
+    "random_bytes",
+    "random_string",
+    "random_dataframe",
+    "random_numpy_array"
 ]
